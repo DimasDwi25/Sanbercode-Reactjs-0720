0e81e9a2bcbbd7a994db0cd41767b581f0a15712
DimasDwi25 <ddwi673@gmail.com>
2020-07-24 10:34:56 +0700

    Tugas hari ke 5

---
+++ Pekan 1/Tugas hari ke 5/tugas5.js
@@ -0,0 +1,50 @@
+
+//Soal nomor 1
+
+//Tulis code function di sini
+function halo() {
+
+    return "Halo Sanbers!"
+
+}
+
+console.log(halo());
+// "Halo Sanbers!"
+
+
+// Soal Nomor 2
+
+//Tulis code function di sini
+function kalikan(num1, num2) {
+
+    return num1 * num2
+
+}
+
+var num1 = 12
+var num2 = 4
+
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali);
+// 48
+
+
+//Soal nomor 3
+
+//Tulis kode function di sini
+function introduce(name, age, address, hobby) {
+
+    return ("Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobbi yaitu " + hobby);
+
+}
+
+var name = "John"
+var age = 30
+var address = "Jalan belum jadi"
+var hobby = "Gaming"
+
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan);
+/*Menampilkan "Nama saya John, umur saya 30 tahun, alamat saya di jalan belum
+jadi, dan saya punya hobby yaitu Gaming!" */
+
